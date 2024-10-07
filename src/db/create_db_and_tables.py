e02c2e7d53cd27304caf9ff31637b8b2fd71cc75
Pavel Bramanis <bramanis2@gmail.com>
2024-10-07 19:41:53 +0300

    feat: Переписал создание моделей на ООП стиль

--- src/db/create_db_and_tables.py
+++ src/db/create_db_and_tables.py
@@ -1,4 +1,6 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import as_declarative, Mapped, mapped_column, Session
+
 from src.config import db_config
 from mysql.connector import Error, connect
 
@@ -16,22 +18,49 @@ from mysql.connector import Error, connect
 
 
 meta = sa.MetaData()
-
-players = sa.Table(
-    'Players', meta,
-    sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('name', sa.VARCHAR(255), nullable=False, unique=True)
-)
-
-matches = sa.Table(
-    'Matches', meta,
-    sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('UUID', sa.String(255), nullable=False, unique=True),
-    sa.Column('Player1', sa.Integer, sa.ForeignKey('Players.id')),
-    sa.Column('Player2', sa.Integer, sa.ForeignKey('Players.id')),
-    sa.Column('Winner', sa.Integer, sa.ForeignKey('Players.id')))
-
+#
+# players = sa.Table(
+#     'Players', meta,
+#     sa.Column('id', sa.Integer, primary_key=True),
+#     sa.Column('name', sa.VARCHAR(255), nullable=False, unique=True)
+# )
+#
+# matches = sa.Table(
+#     'Matches', meta,
+#     sa.Column('id', sa.Integer, primary_key=True),
+#     sa.Column('UUID', sa.String(255), nullable=False, unique=True),
+#     sa.Column('Player1', sa.Integer, sa.ForeignKey('Players.id')),
+#     sa.Column('Player2', sa.Integer, sa.ForeignKey('Players.id')),
+#     sa.Column('Winner', sa.Integer, sa.ForeignKey('Players.id')),
+#     sa.Column('Score', sa.String(255), nullable=False))
+#
 engine = sa.create_engine(f'mysql+mysqlconnector://root:root@localhost:3306/{db_config["mysql"]["name"]}', echo=True)
+#
+# meta.create_all(engine)
+@as_declarative(metadata=meta)
+class AbstractModel:
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
 
-meta.create_all(engine)
 
+class PlayersModel(AbstractModel):
+    __tablename__ = 'Players'
+
+    name: Mapped[str] = mapped_column(sa.VARCHAR(255), nullable=False, unique=True)
+
+class MatchesModel(AbstractModel):
+    __tablename__ = 'Matches'
+
+    UUID: Mapped[str] = mapped_column(nullable=False, unique=True)
+    Player1: Mapped[int] = mapped_column(sa.ForeignKey('Players.id'))
+    Player2: Mapped[int] = mapped_column(sa.ForeignKey('Players.id'))
+    Winner: Mapped[int] = mapped_column(sa.ForeignKey('Players.id'))
+    Score: Mapped[str] = mapped_column(nullable=False)
+
+
+# with Session(engine) as session:
+#     with session.begin():
+#         AbstractModel.metadata.create_all(engine)
+# with Session(engine) as session:
+#     with session.begin():
+#         user = PlayersModel(name='Nadal')   # noqa
+#         session.add(user)
