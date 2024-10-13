870c720c5fd3b9d5f04410c57d4a7161074b021e
Pavel Bramanis <bramanis2@gmail.com>
2024-10-13 18:48:09 +0300

    feat: добавление хендлера для начальной страницы

---
+++ src/handlers/index_handler.py
@@ -0,0 +1,22 @@
+from jinja2 import Template
+
+from src import config
+
+
+class IndexHandler:
+
+    def __call__(self):
+        with open(f'{config.paths_list["templates_files"]}{config.paths_list["index"]}', 'rb') as file:
+            content_before = file.read().decode('utf-8')
+
+            request_uri = f'{config.paths_list["templates_files"]}' \
+                          f'{config.paths_list["index"]}'
+
+
+
+            temlate = Template(content_before)
+
+            content_afer = temlate.render(
+                request_uri=request_uri,
+            )
+            return content_afer
