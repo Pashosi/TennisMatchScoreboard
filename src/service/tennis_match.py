7634b7696301ab2b8e13f18433fa23848243dec3
Pavel Bramanis <bramanis2@gmail.com>
2024-10-24 20:17:31 +0300

    fix: оптимизация кода подсчета очков

--- src/service/tennis_match.py
+++ src/service/tennis_match.py
@@ -22,92 +22,51 @@ class TennisMatch:
         self.tiebreak = None
 
     def add_point(self, point):
+        """
+        Добавление очка игроку.
+        :param point: Словарь с информацией кому добавлять очки. {'name': имя, 'value': поинты}.
+        :raises ValueError: Если матч уже завершен.
+        """
         if self.winner:
             raise ValueError('Матч уже завершен')
 
-        if point['name'] == 'player1':
+        player_name = point['name']
+
+        if player_name == 'player1':
             self.player1_score_point += int(point['value'])
 
-            if self.player1_score_point == 5 and self.player2_score_point == 3 and not self.tiebreak:  # счет AD:40
-                self.player1_score_game += 1
-                self.player1_score_point = 0
-                self.player2_score_point = 0
-            elif self.player1_score_point == 4 and self.player2_score_point == 4 and not self.tiebreak:  # 40->AD:AD
-                self.player1_score_point = 3
-                self.player2_score_point = 3
-            elif self.player1_score_point > 3 > self.player2_score_point and not self.tiebreak:
-                self.player1_score_game += 1
-                self.player1_score_point = 0
-                self.player2_score_point = 0
+            self.process_points(player_name)
 
-            # тайбрейк
-            elif self.player1_score_point == 7 and self.player2_score_point <= 5 and self.tiebreak:  # счет 7:<=5
-                self.player1_score_game += 1
-                self.player1_score_point = 0
-                self.player2_score_point = 0
-            elif self.player1_score_point > 7 and self.player2_score_point <= int(self.player1_score_point) - 2 and self.tiebreak:
-                self.player1_score_game += 1
-                self.player1_score_point = 0
-                self.player2_score_point = 0
             # геймы
             if self.player1_score_game == 6 and self.player2_score_game < 5:  # счет 6:<=4
                 self.player1_score_set += 1
-                self.player1_score_game = 0
-                self.player2_score_game = 0
+                self.reset_score_games()
             elif self.player1_score_game == 7 and self.player2_score_game in [5, 6]:  # счет 7:5, 7:6
                 self.player1_score_set += 1
-                self.player1_score_game = 0
-                self.player2_score_game = 0
+                self.reset_score_games()
                 self.tiebreak = False
             elif self.player1_score_game == 6 and self.player2_score_game == 6:  # счет 6:6
                 self.tiebreak = True
 
-            # сеты
-            if self.player1_score_set == 2 and self.player2_score_set < 2:
-                self.winner = self.player1
-                print(f'победитель {self.winner}')
-        elif point['name'] == 'player2':
+            self.check_match_winner()
+
+        elif player_name == 'player2':
             self.player2_score_point += int(point['value'])
 
-            if self.player2_score_point == 5 and self.player1_score_point == 3 and not self.tiebreak:
-                self.player2_score_game += 1
-                self.player2_score_point = 0
-                self.player1_score_point = 0
-            elif self.player2_score_point == 4 and self.player1_score_point == 4 and not self.tiebreak:  # счет 40->AD:AD
-                self.player2_score_point = 3
-                self.player1_score_point = 3
-            elif self.player2_score_point > 3 > self.player1_score_point and not self.tiebreak:
-                self.player2_score_game += 1
-                self.player2_score_point = 0
-                self.player1_score_point = 0
-
-            # тайбрейк
-            elif self.player2_score_point == 7 and self.player1_score_point <= 5 and self.tiebreak:
-                self.player2_score_game += 1
-                self.player2_score_point = 0
-                self.player1_score_point = 0
-            elif self.player2_score_point > 7 and self.player1_score_point <= int(self.player2_score_point) - 2 and self.tiebreak:
-                self.player2_score_game += 1
-                self.player2_score_point = 0
-                self.player1_score_point = 0
+            self.process_points(player_name)
 
             # геймы
             if self.player2_score_game == 6 and self.player1_score_game < 5:
                 self.player2_score_set += 1
-                self.player2_score_game = 0
-                self.player1_score_game = 0
+                self.reset_score_games()
             elif self.player2_score_game == 7 and self.player1_score_game in [5, 6]:
                 self.player2_score_set += 1
-                self.player2_score_game = 0
-                self.player1_score_game = 0
+                self.reset_score_games()
                 self.tiebreak = False
             elif self.player2_score_game == 6 and self.player1_score_game == 6:
                 self.tiebreak = True
 
-            # сеты
-            if self.player2_score_set == 2 and self.player1_score_set < 2:
-                self.winner = self.player2
-                print(f'победитель {self.winner}')
+            self.check_match_winner()
 
     def update_match_in_bd(self):
         """Обновление данных матча в БД"""
@@ -123,3 +82,61 @@ class TennisMatch:
                             "points": self.player2_score_point}}
 
         return data
+
+    def reset_score_points(self):
+        """Очистка полей счета поинтов"""
+        self.player1_score_point = 0
+        self.player2_score_point = 0
+
+    def reset_score_games(self):
+        """Очистка полей счета геймов"""
+        self.player1_score_game = 0
+        self.player2_score_game = 0
+
+    def check_match_winner(self):
+        """Проверка победителя матча"""
+        if self.player1_score_set == 2:
+            self.winner = self.player1
+            # print(f'Победитель: {self.winner}')
+        elif self.player2_score_set == 2:
+            self.winner = self.player2
+            # print(f'Победитель: {self.winner}')
+
+
+    def process_points(self, player):
+        """Обработка поинтов и геймов"""
+        if player == 'player1':
+            opponent_points = self.player2_score_point
+            player_points = self.player1_score_point
+        else:
+            opponent_points = self.player1_score_point
+            player_points = self.player2_score_point
+
+        # Проверка на выигрыш гейма
+        if not self.tiebreak:
+            if player_points == 5 and opponent_points == 3:  # AD:40
+                self.add_point_game(player)
+            elif player_points > 3 > opponent_points:  # 40:меньше
+                self.add_point_game(player)
+            elif player_points == 4 and opponent_points == 4:  # 40:40
+                self.player1_score_point = 3
+                self.player2_score_point = 3
+
+        # тайбрейк
+        elif self.tiebreak:
+            if player_points == 7 and opponent_points <= 5 and self.tiebreak:
+                self.add_point_game(player)
+                self.reset_score_points()
+            elif player_points > 7 and opponent_points <= int(player_points) - 2:
+                self.add_point_game(player)
+                self.reset_score_points()
+
+    def add_point_game(self, player: str):
+        """Добавление поинта в гейм"""
+        if player == 'player1':
+            self.player1_score_game += 1
+        else:
+            self.player2_score_game += 1
+
+        self.reset_score_points()
+
