fbf7bfa076f2c422e73d51c5be1c4b8f8d76f70f
Pavel Bramanis <bramanis2@gmail.com>
2024-10-26 22:26:28 +0300

    fix: перенос запуска проги в manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,27 @@
-def run():
-    print('Hi')
+from whitenoise import WhiteNoise
+
+import config
+from waitress import serve
+
+from src.router import Router
+
+
+def app(environ, start_response):
+    # Определяем путь запроса
+    path = environ.get('PATH_INFO', '/').lstrip('/')
+    # Метод запроса
+    method = environ.get('REQUEST_METHOD', 'GET')
+
+    if path == 'favicon.ico':
+        # Игнорируем запрос на favicon.ico, возвращаем код 204 (No Content)
+        start_response('204 No Content', [])
+        return []
+
+    router = Router(environ, start_response)
+    response = router(method, path)
+    return response
 
 
 if __name__ == '__main__':
-    run()
+    application = WhiteNoise(app, config.paths_list['static_files'])
+    serve(application, host='localhost', port=8080)
